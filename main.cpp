5c0057ae535d32f0113ac0cc757f866f1620e280
Gofnik <stalker75asd@gmail.com>
2025-12-07 15:46:50 +0500

    Add Write_In_File

--- main.cpp
+++ main.cpp
@@ -22,7 +22,25 @@ void Gpu_Table(const auto& gpus){
 
 }
 void Write_In_File(const auto& gpus){
-
+    std::ofstream file("gpus.bin", std::ios::binary);
+    if (!file.is_open()){
+        std::cout<< "Ошибка открытия файла для записи" << std::endl;
+        return;
+    }
+    std::stringstream ss;
+    int64_t cnt = 0;
+    for (const auto& gpu : gpus){
+        cnt++;
+        ss << "Видеокарта №" << cnt << "\n";
+        ss << "Название: " << gpu.name << "\n";
+        ss << "Техпроцесс: " << gpu.technical_process << "\n";
+        ss << "Базовая частота: " << gpu.frequency_base << "\n";
+        ss << "Количество видеопамяти: " << gpu.video_memory << "\n";
+    }
+    std::string minfo = ss.str();
+    file.write(minfo.c_str(), minfo.size());
+    file.close();
+    return;
 }
 int main(){
     std::vector<gpu> gpus = Read_Gpus_From_File("gpus.txt");
