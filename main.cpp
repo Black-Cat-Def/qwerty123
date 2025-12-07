24385eef572e89b392905a13fec7d5be52c4f017
AndreyK <kyrutnikovandrew2006@gmail.com>
2025-12-07 14:44:47 +0500

    Jeka_Krutoy_0.1

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+
+
+using namespace std;
+
+void Read_File(const auto& file_name){
+}
+
+void In_File(const auto& file_name){
+}
+
+
+void Write_In_File(const auto& gpus){
+}
+
+int main(){
+    Read_File("123");
+    In_File("123");
+    Write_In_File("123");
+    return 0;
+}
