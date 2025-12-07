51f9a86c62635ad8e9183b4dcebf0af748a30f64
e_k <redcatfox2018@gmail.com>
2025-12-07 15:12:51 +0500

    Add Gpu_Table

--- main.cpp
+++ main.cpp
@@ -19,7 +19,23 @@ std::vector<gpu> Read_Gpus_From_File(const auto& file_name){
     return gpus;
 }
 void Gpu_Table(const auto& gpus){
-
+    std::cout <<"\n" << std::setw(65) << "ТАБЛИЦА ВИДЕОКАРТ\n";
+    std::cout << std::string(80, '-') << std::endl;
+    std::cout << std::left
+        << std::setw(35) << "Название"
+        << std::setw(23) << "Техпроцесс"
+        << std::setw(32) << "Базовая частота"
+        << "Количество видеопамяти" << std::endl;
+    std::cout << std::string(80, '-') << std::endl;
+    for (const auto& gpu : gpus) {
+        std::cout << std::left
+             << std::setw(31) << gpu.name
+             << std::setw(15) << gpu.technical_process
+             << std::setw(20) << gpu.frequency_base
+             << gpu.video_memory
+             << std::endl;
+             std::cout << std::string(80, '-') << std::endl;
+    }
 }
 void Write_In_File(const auto& gpus){
 
