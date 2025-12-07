a57b11754db8ee90123943e1b660fbe35899f956
AndreyK <kyrutnikovandrew2006@gmail.com>
2025-12-07 15:09:06 +0500

    Add in Jeka_Krutoy Read_Gpus_From_File

--- main.cpp
+++ main.cpp
@@ -16,6 +16,23 @@ struct gpu{
 };
 
 std::vector<gpu> Read_Gpus_From_File(const auto& file_name){
+    std::vector<gpu> gpus;
+    std::ifstream gpu_file(file_name);
+    if (!gpu_file.is_open()){
+        std::cout << "Не удалось открыть файл" << std::endl;
+        return gpus;
+    }
+
+    gpu in_gpu;
+    while (!gpu_file.eof()){
+        gpu_file >> in_gpu.name;
+        gpu_file >> in_gpu.technical_process;
+        gpu_file >> in_gpu.frequency_base;
+        gpu_file >> in_gpu.video_memory;
+        gpus.push_back(in_gpu);
+    }
+
+    gpu_file.close();
     return gpus;
 }
 void Gpu_Table(const auto& gpus){
